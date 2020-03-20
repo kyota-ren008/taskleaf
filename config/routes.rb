5cb66ceb27e3fe8c5660de4c0806943d3c6f4530
“RenHamamoto” <rentaro0831@gmail.com>
2020-03-20 21:29:00 +0900

    確認画面機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
-  
+
   root to: 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
+  end
 end
