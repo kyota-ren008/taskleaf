0650a47de09ba137fbe09104a93e4c9989405bea
“RenHamamoto” <rentaro0831@gmail.com>
2020-03-20 18:43:29 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.find_or_create_by!(email: 'admin@example.com') do |user|
-  user.name: 'admin',
-  user.email: 'admin@example.com',
-  user.admin: true,
-  user.password: 'password',
+  user.name: 'admin'
+  user.email: 'admin@example.com'
+  user.admin: true
+  user.password: 'password'
   user.password_confirmation: 'password'
 end
